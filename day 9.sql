574020e08351cda6d775b3fe769cae106808d93d
nehagjr <pneha3169@gmail.com>
2024-07-30 18:45:50 +0530

    day 9

---
+++ day 9.sql
@@ -0,0 +1,16 @@
+use neha;
+create table orders(id int primary key auto_increment,name varchar(20),mobile varchar(20));
+desc orders;
+insert into orders(name,mobile) values("hij","46546"),("rger","5455465"),("ererf","3465465");
+select * from orders;
+insert into orders(name,mobile) values("sdfd","46555446"),("rdfr","54656"),("dcds","4545");
+delete from orders where id=5;
+
+#creating variable here ...here @ define it is an user define variable
+set @num=0;
+
+update orders set id=@num:=@num+1;
+set sql_safe_updates=0;
+
+alter table orders auto_increment=100; 
+drop table orders;
