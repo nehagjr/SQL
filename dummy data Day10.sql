f47a9bab20a55f3aef18267c87f9c9e4b1b51cf7
Neha patel <pneha3169@gmail.com>
2024-09-13 16:39:42 +0530

    rollup

--- dummy data Day10.sql
+++ dummy data Day10.sql
@@ -2,6 +2,7 @@ use dummy;
 show tables;
 desc products;
 
+create table sales
 select year(orderdate) as order_year, productLine, sum(quantityordered*priceeach) as total_order_value from
 orders o
 inner join
@@ -10,3 +11,24 @@ using (ordernumber)
 inner join products p
 using (productcode)
 group by year(orderdate),productLine;
+
+select * from sales;
+
+select productLine , sum(total_order_value) from
+sales group by productLine;
+
+select sum(total_order_value)from sales;
+
+#merge both above------------------
+select productLine , sum(total_order_value) from
+sales group by productLine
+union
+select null,sum(total_order_value)from sales;
+
+# rollup is a keyword ----> do supper aggreate -------by default 
+select productLine , sum(total_order_value) from
+sales group by productLine with rollup;
+
+#w sql qary that  return total order value by product line and order year
+select productLine, ifnull(order_year,"total") ,sum(total_order_value) from
+sales group by productLine, order_year  with rollup;
